431ee885be6c62cd1a319d2f270424fcad344040
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2023-03-01 11:34:47 +0600

    File Added

---
+++ EduHubEntiry/UniversityDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHubEntity
+{
+    public class UniversityDetail : Entity
+    {      
+        public string UniversityName { get; set; }
+        public string CountryName { get; set; }
+        public string CityName { get; set; }
+        public string SubjectName { get; set; }
+        public long TuitionFees { get; set; }
+        public string IELTSScoreRequirement { get; set; }
+        public string GREScoreRequirement { get; set; }
+        public string TOFELScoreRequirement { get; set; }
+        public string UniversityQSWorldRank { get; set; }
+        public string MinimumCGPARequirement { get; set; }
+        public int CourseType { get; set; }
+        public string AdditonalRequirements { get; set; }
+    }
+}
