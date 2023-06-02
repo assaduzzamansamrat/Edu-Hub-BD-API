5e3dcee6bab18d10926eff9a3bcce08c1602fa77
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2023-06-02 20:37:47 +0600

    Update

---
+++ EduHubBd/Controllers/SignUpController.cs
@@ -0,0 +1,40 @@
+﻿using EduHubEntity;
+using EduHubInterface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EduHubBd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]  
+    public class SignUpController : ControllerBase
+    {
+        private IUserRepository userRepo;
+
+        public SignUpController(IUserRepository userRepo)
+        {
+            this.userRepo = userRepo;
+        }
+        [Route("SignUp")]
+        [HttpPost]
+        public async Task<IActionResult> SignUp(User user)
+        {
+            try
+            {
+                if(ModelState.IsValid == true)
+                {
+                    await this.userRepo.InsertAsync(user);
+                    return Ok(new { result = true });
+                }
+                return Ok(new { result = false });
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+       
+    }
+}
---
+++ EduHubBd/Controllers/SuperAdmin/SuperAdminController.cs
@@ -0,0 +1,34 @@
+﻿using EduHubEntity;
+using EduHubInterface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHubBd.Controllers.SuperAdmin
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuperAdminController : ControllerBase
+    {
+        private IUserRepository userRepo;
+
+        public SuperAdminController(IUserRepository userRepo)
+        {
+            this.userRepo = userRepo;
+        }
+        [Route("GetAllUsers")]
+        [HttpGet()]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                List<User> users = await this.userRepo.GetAllAsync();
+                return Ok(new { users });
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+     
+    }
+}
