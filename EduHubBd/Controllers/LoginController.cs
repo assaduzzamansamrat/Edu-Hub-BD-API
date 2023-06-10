365078b8454bb60bee5fe5762621bcfe1285697c
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2023-06-10 12:52:35 +0600

    Update

---
+++ EduHubBd/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHubBd.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
